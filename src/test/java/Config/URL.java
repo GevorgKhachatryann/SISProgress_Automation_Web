43f56e72953789fda8ded5bcaf912b942736a512
GevorgKhachatryann <gevorgkhachatryan285@gmail.com>
2023-06-23 11:13:09 +0400

    fix

--- src/test/java/Config/URL.java
+++ src/test/java/Config/URL.java
@@ -5,7 +5,6 @@ public class URL {
         public static final String APP_URL = "https://sisprogress.com/";
         public static final String Login_URL = "https://sisprogress.com/getstarted";
         public static final String Registration_URL = "https://sisprogress.com/register";
-
         public static final String MAIL_API_BASE_URL = "https://api.mail.tm";
         public static final String FINANCIAL_AID = "https://sisprogress.com/financialaid";
 }
